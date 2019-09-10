592f29ae9856b99f10c046ea35a81900e24445b8
José Joaquim Júnior <franca.josejunior@outlook.com>
2019-09-10 17:55:56 -0300

    -

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Header from './Header'
+import { Formik } from 'formik'
 import CardDetailsContainer from '../containers/CardDetailsContainer'
+import { TextField } from '@material-ui/core'
 
 const CardDetails = ({ card, itinerary }) => {
-  return <>CardDetails</>
+  console.log(card)
+  return (
+    <Formik
+      initialValues={{ ...card }}
+      render={({ handleChange, handleBlur, values }) =>
+        console.log({ values }) || (
+          <>
+            <TextField
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </>
+        )
+      }
+    />
+  )
 }
 
 export default CardDetails
