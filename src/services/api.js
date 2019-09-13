898a346139c471bad33779c8b15a1dec39173eeb
José Joaquim Júnior <franca.josejunior@outlook.com>
2019-09-13 17:24:08 -0300

    https://francielle-api.herokuapp.com

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://francielle-api.herokuapp.com'
 })
 
 api.interceptors.response.use(response => response.data || response)
